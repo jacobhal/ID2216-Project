409afca8c5d19e320324235a388e0aa305ee9505
Nezz94 <nedo@kth.se>
2018-02-22 16:45:36 +0100

    Fixed NavDrawer

--- vueSplit/src/components/NavDrawer.vue
+++ vueSplit/src/components/NavDrawer.vue
@@ -11,10 +11,31 @@
       v-model="openDrawer"
       absolute
     >
-      <v-list>
-        <v-list-tile>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Menu
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list two-line>
+        <v-list-tile @click="routeToWelcomeScreen">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title><v-icon>home</v-icon>Home</v-list-tile-title>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="routeToAllTab">
+          <v-list-tile-action>
+            <v-icon>assignment</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Tabs</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -27,10 +48,28 @@ export default {
   name: 'NavDrawer',
   data () {
     return {
-      openDrawer: false
+      openDrawer: false,
+      pathToAllTabs: '/allTabs',
+      pathToWelcomeScreen: '/'
     }
   },
   methods: {
+    routeToAllTab: function () {
+      if (this.$route.name !== 'AllTabs') {
+        this.$router.push(this.pathToAllTabs)
+      } else {
+        this.openDrawer = false
+      }
+    },
+
+    routeToWelcomeScreen: function () {
+      if (this.$route.name !== 'WelcomeScreen') {
+        this.$router.push(this.pathToWelcomeScreen)
+      } else {
+        this.openDrawer = false
+      }
+    }
+
   }
 }
 </script>
