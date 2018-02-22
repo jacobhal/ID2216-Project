98e6cd3a39c5d208eb9b782bc246e84314de306f
Nezz94 <nedo@kth.se>
2018-02-22 15:29:45 +0100

    NavDrawer component added

--- vueSplit/src/App.vue
+++ vueSplit/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <v-app>
+    <nav-drawer></nav-drawer>
     <v-content>
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
   </v-content>
   </v-app>
 </template>
 
 <script>
+import NavDrawer from '@/components/NavDrawer'
 export default {
   data () {
     return {
+      openDrawer: false
     }
+  },
+  components: {
+    NavDrawer
   },
   name: 'App'
 }
---
+++ vueSplit/src/components/NavDrawer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-toolbar app>
+      <v-btn icon @click="openDrawer = !openDrawer">
+        <v-icon>list</v-icon>
+    </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="openDrawer"
+      absolute
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title><v-icon>home</v-icon>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavDrawer',
+  data () {
+    return {
+      openDrawer: false
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
