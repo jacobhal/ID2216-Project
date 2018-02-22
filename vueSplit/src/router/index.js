7aeb81fe9e09982edb8fed1c92ba1c4ac9332a89
Jacob <jackeaik@hotmail.com>
2018-02-22 17:09:22 +0100

    Feat: Added Vuex to project

--- vueSplit/src/router/index.js
+++ vueSplit/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import WelcomeScreen from '@/components/WelcomeScreen'
 import HomeScreen from '@/components/HomeScreen'
-import AllTabs from '@/components/AllTabs'
+import AllTabs from '@/components/AllTabsScreen'
 
 Vue.use(Router)
 
@@ -15,7 +15,7 @@ export default new Router({
     },
     {
       path: '/allTabs',
-      name: 'AllTabs',
+      name: 'AllTabsScreen',
       component: AllTabs
     },
     {
