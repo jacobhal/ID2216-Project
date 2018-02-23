71a0a904303e697657c0b2445e68a22c15279c98
Nezz94 <nedo@kth.se>
2018-02-23 18:07:14 +0100

    First version of screen for inputting amounts

--- vueSplit/src/App.vue
+++ vueSplit/src/App.vue
@@ -2,9 +2,7 @@
   <v-app>
     <nav-drawer></nav-drawer>
     <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
+      <router-view></router-view>
   </v-content>
   </v-app>
 </template>
