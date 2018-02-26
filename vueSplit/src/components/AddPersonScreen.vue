aa42718305a212f5aa819dbf443209f48ac080b0
Nezz94 <nedo@kth.se>
2018-02-26 21:11:12 +0100

    AddPersonScreen done, but bug in store preventing testing

--- vueSplit/src/components/AddPersonScreen.vue
+++ vueSplit/src/components/AddPersonScreen.vue
@@ -15,25 +15,36 @@
       <v-flex xs12>
         <v-card>
           <v-card-text>
-            <v-text-field
-              label="Name"
-              :rules="nameRules"
-              v-model="personName"
-            ></v-text-field>
-            <v-text-field
-              label="Phone number"
-              :rules="phoneRules"
-              v-model="personPhoneNumber"
-            ></v-text-field>
+            <v-form v-model="valid">
+              <v-text-field
+                label="Name"
+                :rules="nameRules"
+                v-model="personName"
+                v-on="{ blur: updateNextButtonColor }"
+              ></v-text-field>
+              <v-text-field
+                label="Phone number"
+                :rules="phoneRules"
+                v-model="personPhoneNumber"
+                v-on="{ blur: updateNextButtonColor }"
+              ></v-text-field>
+            </v-form>
           </v-card-text>
         </v-card>
       </v-flex>
+    </v-layout>
+    <!-- Submit button, only red when all input fields are valid -->
+    <v-layout row align-content-center justify-center>
+      <v-btn class="submitContainer" v-bind:color="color" large fab dark @click="submit">
+        <v-icon>check</v-icon>
+      </v-btn>
     </v-layout>
   </v-container>
 </template>
 
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'AddPersonScreen',
   data () {
@@ -41,6 +52,8 @@ export default {
       tabId: this.$route.params.id,
       personName: '',
       personPhoneNumber: '',
+      color: 'grey',
+      valid: false,
       nameRules: [
         () => this.personName.length > 0 || 'Field is required'
       ],
@@ -52,7 +65,45 @@ export default {
   methods: {
     goBack: function () {
       this.$router.back()
+    },
+    updateNextButtonColor: function () {
+      if (this.valid) {
+        this.color = 'red'
+      } else {
+        this.color = 'grey'
+      }
+    },
+    submit: function () {
+      if (this.valid) {
+        // First add the person to the store
+        this.addPerson()
+        // Then add the person to the tab
+        const personId = this.persons.slice(-1)[0].id // Need to get id of the person added
+        this.addPersonToTab(personId)
+        // Person has been added, return to AddReceiptScreen
+        this.$router.push({ name: 'AddReceiptScreen', params: { id: this.tabId } })
+      } else {
+        alert('You must input a valid name and phone number') // TODO: Handle this better
+      }
+    },
+    addPerson: function () {
+      this.$store.dispatch('addPerson', {
+        name: this.personName,
+        phoneNumber: this.personPhoneNumber
+      })
+    },
+    addPersonToTab: function (pId) {
+      this.$store.dispatch('addPersonToTab', {
+        tabId: this.tabId,
+        personId: pId
+      })
     }
+  },
+  computed: {
+    ...mapGetters([
+      'persons',
+      'tabById'
+    ])
   }
 }
 </script>
@@ -60,5 +111,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.submitContainer {
+  margin-top: 10%
+}
 </style>
