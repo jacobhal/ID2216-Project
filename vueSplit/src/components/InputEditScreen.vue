7daf3b27dd75790403e4849f449a82a348ddb08d
Nezz94 <nedo@kth.se>
2018-02-28 20:37:41 +0100

    something wrong with store

--- vueSplit/src/components/InputEditScreen.vue
+++ vueSplit/src/components/InputEditScreen.vue
@@ -23,13 +23,14 @@
                   <v-list-tile-title>{{ person.name }}</v-list-tile-title>
                 </v-list-tile-content>
                 <v-list-tile-action>
-                  <v-form>
+                  <v-form v-model="validFields[index]">
                     <v-text-field
                       label="Amount"
                       full-width
                       append-icon="delete"
                       :value="getPurchaseOfPerson(person.id).price"
                       :rules="inputRules"
+                      v-model="amounts[index]"
                     ></v-text-field>
                   </v-form>
                 </v-list-tile-action>
@@ -59,13 +60,25 @@ export default {
       inputRules: [
         // () => !!this.amounts[0] || 'This field is required',
         v => /^\d+(\.\d+)?$/.test(v) || 'Field can only contain numbers'
-      ]
+      ],
+      amounts: [], // Inputted amount, tied to form
+      validFields: [] // boolean for if input field is valid
     }
   },
   created: function () {
     const receipt = this.$store.getters.receiptById(this.receiptId)
-    this.persons = this.$store.getters.receiptPersons(receipt)
+    console.log(receipt)
+    this.persons = receipt.persons
+    // this.persons = this.$store.getters.receiptPersons(receipt)
+    // console.log(this.persons)
     this.purchases = this.$store.getters.receiptPurchases(receipt)
+    console.log(this.purchases)
+    for (var i = 0; i < this.persons.length; i++) {
+      // Fill amounts with null since nothing has been inputted yet. Needed for v-model
+      this.amounts.push(null)
+      // Fill validFields with false since all are invalid at first. needed for v-model
+      this.validFields.push(false)
+    }
   },
   methods: {
     goBack: function () {
